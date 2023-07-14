b5f14efbe1c8ea19bc5b199137a3c792a49d22f5
masato <spmt3710@gmail.com>
2023-07-14 17:11:14 +0900

    update : notice はをやめる

--- paperswithcode_nigiri/routers/paper.py
+++ paperswithcode_nigiri/routers/paper.py
@@ -1,8 +1,5 @@
-import os
-
 from fastapi import APIRouter
 
-from paperswithcode_nigiri.services.notifier import notice_slack
 from paperswithcode_nigiri.services.searcher import search_papers
 from paperswithcode_nigiri.services.summarizer import make_summary
 
@@ -14,15 +11,7 @@ async def get_papers(
     keyword: str,
     page: int = 1,
     items_per_page: int = 1,
-    notice: None | str = None,
 ):
     papers = search_papers(keyword, page=page, items_per_page=items_per_page)
-    summaries = []
-    for paper in papers:
-        summary = make_summary(paper)
-
-        if notice is not None:
-            if notice == "slack":
-                notice_slack(webhook_url=os.getenv("SLACK_WEBHOOK_URL"), message=summary)
-        summaries.append(summary)
+    summaries = [make_summary(paper) for paper in papers]
     return summaries
