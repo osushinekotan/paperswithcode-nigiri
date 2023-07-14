e21b13f062600f5e4b4aa34cccb30cf5aa6188c2
masato <spmt3710@gmail.com>
2023-07-14 22:48:24 +0900

    update : api の使用を変更

--- api/app/routers/summary.py
+++ api/app/routers/summary.py
@@ -1,15 +1,17 @@
 from fastapi import APIRouter
 
 from app.services.searcher import search_papers
+from app.services.summarizer import make_summary
 
 router = APIRouter()
 
 
-@router.get("/paper/", response_model=list[dict])
-async def get_paper(
+@router.get("/summary/", response_model=list[dict])
+async def get_summary(
     keyword: str,
     page: int = 1,
     items_per_page: int = 1,
 ):
     papers = search_papers(keyword, page=page, items_per_page=items_per_page)
-    return papers
+    summaries = [make_summary(paper) for paper in papers]
+    return summaries
